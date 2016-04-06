965f7a62360528e5488c7ebd5f4244ad5ef5faa0
Clay Carpenter <claycarpenter@gmail.com>
2016-04-06 09:45:50 -0400

    Creates Dashboard tab.
    Removes starter template tabs.

---
+++ app/pages/dashboard/dashboard.ts
@@ -0,0 +1,10 @@
+import {Page} from 'ionic-angular';
+
+@Page({
+  templateUrl: 'build/pages/dashboard/dashboard.html'
+})
+export class Dashboard {
+  constructor() {
+    // Do nothing right now...
+  }
+}
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,7 +1,5 @@
 import {Page} from 'ionic-angular';
-import {Page1} from '../page1/page1';
-import {Page2} from '../page2/page2';
-import {Page3} from '../page3/page3';
+import {Dashboard} from '../dashboard/dashboard';
 
 
 @Page({
@@ -10,7 +8,5 @@ import {Page3} from '../page3/page3';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = Page1;
-  tab2Root: any = Page2;
-  tab3Root: any = Page3;
+  dashboardTabRoot: any = Dashboard;
 }
